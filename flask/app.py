8fd467a3cd486a270f4651f278f6667bf789b7ac
xyw <2506697596@qq.com>
2024-05-04 19:07:03 +0800

    Changes

--- flask/app.py
+++ flask/app.py
@@ -14,7 +14,7 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/pubmed"
 mongo = PyMongo(app)
 
 df = None
-
+##连接数据库并获取数据
 def get_data_from_mongodb():
     global df  # 声明 df 为全局变量
     try:
@@ -30,7 +30,7 @@ def get_data_from_mongodb():
         print("Error while getting data from MongoDB:", e)
         return None
 
-
+##存储数据到全局变量
 def load_data():
     # 加载数据并保存到全局变量 df 中
     global df
@@ -47,27 +47,26 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
 
+##数据库记录计数
 @app.route('/counts')
 def data_counts():
     global df
 
-    # 获取清洗前的数据量
+    #退稿数据计数
     original_count = mongo.db.pubmed_collection.count_documents({})
-
-    # 获取清洗后的数据量
-    cleaned_count = len(df)
-
+    #通过数据计数
+    pass_count = mongo.db.pubmed_review_collection.count_documents({})
     # 构建返回的 JSON 对象
     counts = {
-        'original_count': original_count,
-        'cleaned_count': cleaned_count
+        're_count': original_count,
+        'pass_count' : pass_count
     }
 
     return jsonify(counts)
 
 
 
-
+##国家计数
 @app.route('/country')
 def data_country():
     global df
@@ -89,7 +88,7 @@ def data_country():
 
     return jsonify(data)
 
-
+##共现矩阵
 @app.route('/Coc')
 def cooccurrence_matrix_with_max_combination_json():
     global df
@@ -143,35 +142,154 @@ def cooccurrence_matrix_with_max_combination_json():
 
     return jsonify(output_data)
 
+##年份计数
+@app.route('/PY')
+def count_by_year():
+    global df
+    # 检查数据是否已加载
+    if df is None:
+        return 'Data not loaded yet', 400
+    # 对 'PY' 字段进行计数统计
+    df_cleaned = df.dropna(subset=['DCOM'])
+    year_counts = df_cleaned['DCOM'].astype(str).str[:4].value_counts().to_dict()
+    # 构建包含年份和计数的字典列表
+    result = [{'年份': year, '计数': count} for year, count in year_counts.items()]
+    return jsonify(result)
 
 
+##期刊年份退稿关系
+@app.route('/PU')
+#读取数据并构建出版社年份退稿关系
+def generate_chart_data():
+    global df
+    df = df.dropna(subset=['DCOM'])
+    df = df.dropna(subset=['TA'])
+    # 计算每个出版社每年的退稿次数
+    publisher_year_counts = defaultdict(lambda: defaultdict(int))
+    for index, row in df.iterrows():
+        publishers = re.sub(r'\([^)]*\)', '', row['TA']).split(' & ')  # 删除括号内的内容
+        year = str(row['DCOM'])[:4]  # 只取年份的前四位数字
+        for publisher in publishers:
+            publisher_year_counts[publisher][year] += 1
+
+    # 计算总退稿次数和阈值
+    total_count = sum(sum(counts.values()) for counts in publisher_year_counts.values())
+    threshold = total_count * 0.02
+
+    # 构建数据
+    years_sorted = sorted(set(str(row['DCOM'])[:4] for _, row in df.iterrows()))  # 年份递增排序
+    chart_data = ['product,' + ','.join(years_sorted)]
+    other_counts = defaultdict(int)
+    for publisher, year_counts in publisher_year_counts.items():
+        if sum(year_counts.values()) >= threshold:
+            data = [str(year_counts.get(year, 0)) for year in years_sorted]
+            chart_data.append(f'{publisher},{",".join(data)}')
+        else:
+            for year, count in year_counts.items():
+                other_counts[year] += count
+
+    # 添加 "other" 行
+    other_data = [str(other_counts[year]) for year in years_sorted]
+    chart_data.append(f'other,{",".join(other_data)}')
+
+    return jsonify(data=chart_data)
+
+#关键词年份关系
+@app.route('/DE')
+def get_de_dcom_relationship():
+    global df
+    df = df.dropna(subset=['DCOM'])
+    df = df.dropna(subset=['MH'])
+    # 创建一个空的字典，用于存储关键词和每年的计数
+    de_dcom_relationship = {}
+
+    # 遍历 DataFrame 中的每一行
+    for index, row in df.iterrows():
+        # 获取关键词列表
+        keywords = row['MH']
+        # 更新关键词和每年的计数
+        for keyword in keywords:
+            de_dcom_relationship.setdefault(keyword, Counter())
+            year = str(row['DCOM'])[:4]  # 只取年份的前四位数字
+            de_dcom_relationship[keyword][year] += 1
+
+    # 取出计数值总数最高的10个关键词
+    top_10_keywords = dict(
+        sorted(de_dcom_relationship.items(), key=lambda item: sum(item[1].values()), reverse=True)[:15])
+
+    # 返回每个关键词每一年的变化情况
+    result = {}
+    for keyword, year_counter in top_10_keywords.items():
+        result[keyword] = dict(year_counter)
+
+    # 返回关系变化数据
+    return jsonify(result)
+
+#标题和被引用次数
+@app.route('/TIZ9')
+def get_echarts_data():
+    global df
+    df = df.dropna(subset=['VI'])
+    df = df.dropna(subset=['MH'])
+    # 将Z9列作为纵坐标轴的数据（dataAxi）
+    dataAxis = df['VI'].tolist()
+    # 将TI列作为横坐标轴的数据（data）
+    data = df['TI'].tolist()
+
+    # 创建一个字典，将标题作为键，引用次数作为值
+    chart_data = {}
+    for title, reference_count in zip(dataAxis, data):
+        chart_data[title] = reference_count
+
+    # 按照引用次数降序排序
+    sorted_chart_data = dict(sorted(chart_data.items(), key=lambda item: item[1], reverse=True))
+
+    # 提取前20个标题和对应的引用次数
+    top_20_chart_data = dict(list(sorted_chart_data.items())[:20])
+
+    # 返回ECharts数据
+    return jsonify(top_20_chart_data)
 
 
+##关键词词云图
+@app.route('/wordcloud')
+def generate_wordcloud():
+    global df
+    df = df.dropna(subset=['MH'])
+    # 合并 "MH" 列中的列表内容为一个字符串
+    mh_text = ''
+    for index, row in df.iterrows():
+        # 将列表内容拼接成一个字符串
+        mh_text += ', '.join(row['MH']) + ' '
 
+    # 使用 WordCloud 生成词云图像，并设置背景为透明
+    wordcloud = WordCloud(width=800, height=400, background_color='rgba(0, 0, 0, 0)').generate(mh_text)
 
+    # 保存词云图像到临时文件
+    wordcloud_file = 'wordcloud.png'
+    wordcloud.to_file(wordcloud_file)
 
-# @app.route('/wordcloud')
-# def generate_wordcloud():
-#     global df
-#
-#     # 合并 "DE" 字段和 "WC" 字段中的关键词
-#     de_wc_text = ''
-#     for index, row in df.iterrows():
-#         de_wc_text += row['DE'].replace(' ', ',') + ' ' + row['WC'].replace(' ', ',') + ' '
-#
-#     # 使用 WordCloud 生成词云图像，并设置背景为透明
-#     wordcloud = WordCloud(width=800, height=400, background_color='rgba(0, 0, 0, 0)').generate(de_wc_text)
-#
-#     # 保存词云图像到临时文件
-#     wordcloud_file = 'wordcloud.png'
-#     wordcloud.to_file(wordcloud_file)
-#
-#     # 返回词云图像的 URL
-#     return send_file(wordcloud_file, mimetype='image/png')
+    # 返回词云图像的 URL
+    return send_file(wordcloud_file, mimetype='image/png')
 
+##被退稿最多的作者
+@app.route('/AU')
+def top_rejected_authors():
+    global df
+    df = df.dropna(subset=['FAU'])
+    # 将FAU列展开为单独的行
+    authors = df.explode('FAU')
 
+    # 计算每位作者的退稿次数
+    rejected_authors = authors['FAU'].value_counts()
 
+    # 按退稿次数降序排序
+    sorted_rejected_authors = rejected_authors.sort_values(ascending=False)
 
+    # 返回退稿次数最高的前10位作者及其退稿次数
+    top_rejected_authors = sorted_rejected_authors.head(15).to_dict()
+
+    return jsonify(top_rejected_authors)
 
 
 
@@ -197,164 +315,6 @@ def cooccurrence_matrix_with_max_combination_json():
 #         query = " & ".join([f"{field} == '{value}'" for field, value in query_params.items()])
 #         query_result = df.query(query)
 #         return jsonify(query_result.to_dict(orient='records'))
-
-
-# @app.route('/PY')
-# def count_by_year():
-#     global df
-#     # 检查数据是否已加载
-#     if df is None:
-#         return 'Data not loaded yet', 400
-#     # 对 'PY' 字段进行计数统计
-#     year_counts = df['PY'].value_counts().to_dict()
-#     # 构建包含年份和计数的字典列表
-#     result = [{'年份': year, '计数': count} for year, count in year_counts.items()]
-#     return jsonify(result)
-
-
-# @app.route('/PU')
-# 读取数据并构建出版社年份退稿关系
-
-# def generate_chart_data():
-#     global df
-#
-#     # 计算每个出版社每年的退稿次数
-#     publisher_year_counts = defaultdict(lambda: defaultdict(int))
-#     for index, row in df.iterrows():
-#         publishers = row['PU'].split(' & ')
-#         year = row['PY']
-#         for publisher in publishers:
-#             publisher_year_counts[publisher][year] += 1
-#
-#     # 计算总退稿次数和阈值
-#     total_count = sum(sum(counts.values()) for counts in publisher_year_counts.values())
-#     threshold = total_count * 0.02
-#
-#     # 构建数据
-#     years_sorted = sorted(df['PY'].unique())  # 年份递增排序
-#     chart_data = ['product,' + ','.join(str(year) for year in years_sorted)]
-#     other_counts = defaultdict(int)
-#     for publisher, year_counts in publisher_year_counts.items():
-#         if sum(year_counts.values()) >= threshold:
-#             data = [str(year_counts.get(year, 0)) for year in years_sorted]
-#             chart_data.append(f'{publisher},{",".join(data)}')
-#         else:
-#             for year, count in year_counts.items():
-#                 other_counts[year] += count
-#
-#     # 添加 "other" 行
-#     other_data = [str(other_counts[year]) for year in years_sorted]
-#     chart_data.append(f'other,{",".join(other_data)}')
-#
-#     return jsonify(data=chart_data)
-
-
-#关键词年份关系
-# @app.route('/DE')
-# def get_de_py_relationship():
-#     global df
-#     # 创建一个空的字典，用于存储关键词和每年的计数
-#     de_py_relationship = {}
-#
-#     # 遍历 DataFrame 中的每一行
-#     for index, row in df.iterrows():
-#         # 将 DE 列拆分成单独的关键词
-#         keywords = row['DE'].split('; ')
-#         # 更新关键词和每年的计数
-#         for keyword in keywords:
-#             de_py_relationship.setdefault(keyword, Counter())
-#             year = str(row['PY'])
-#             de_py_relationship[keyword][year] += 1
-#
-#     # 取出计数值总数最高的10个关键词
-#     top_10_keywords = dict(
-#         sorted(de_py_relationship.items(), key=lambda item: sum(item[1].values()), reverse=True)[:10])
-#
-#     # 返回每个关键词每一年的变化情况
-#     result = {}
-#     for keyword, year_counter in top_10_keywords.items():
-#         result[keyword] = dict(year_counter)
-#
-#     # 返回关系变化数据
-#     return jsonify(result)
-
-
-# #标题和被引用次数
-# @app.route('/TIZ9')
-# def get_echarts_data():
-#     global df
-#
-#     # 将Z9列作为纵坐标轴的数据（dataAxi）
-#     dataAxis = df['Z9'].tolist()
-#     # 将TI列作为横坐标轴的数据（data）
-#     data = df['TI'].tolist()
-#
-#     # 创建一个字典，将标题作为键，引用次数作为值
-#     chart_data = {}
-#     for title, reference_count in zip(dataAxis, data):
-#         chart_data[title] = reference_count
-#
-#     # 按照引用次数降序排序
-#     sorted_chart_data = dict(sorted(chart_data.items(), key=lambda item: item[1], reverse=True))
-#
-#     # 提取前20个标题和对应的引用次数
-#     top_20_chart_data = dict(list(sorted_chart_data.items())[:20])
-#
-#     # 返回ECharts数据
-#     return jsonify(top_20_chart_data)
-
-
-
-
-
-# @app.route('/WC')
-# def wc_counts():
-#     # 创建一个字典来统计每个学科的出现次数
-#     category_counts = defaultdict(int)
-#     total_count = 0
-#
-#     # 统计所有学科的出现次数
-#     for categories in df['WC']:
-#         unique_categories = set()
-#         for category in categories.split('; '):
-#             for sub_category in category.split(', '):
-#                 unique_categories.add(sub_category.strip())
-#         total_count += len(unique_categories)
-#         # 统计每个学科的出现次数
-#         for category in unique_categories:
-#             category_counts[category] += 1
-#
-#     # 筛选频次高于百分之三的学科
-#     filtered_category_counts = {category: count for category, count in category_counts.items() if
-#                                 count / total_count > 0.03}
-#
-#     # 计算 "other" 的频次
-#     other_count = total_count - sum(filtered_category_counts.values())
-#     filtered_category_counts['other'] = other_count
-#
-#     # 将结果格式化为所需的格式
-#     result = [{'name': category, 'count': count} for category, count in filtered_category_counts.items()]
-#
-#     return jsonify(result)
-
-
-# @app.route('/PI')
-# def count_by_PI():
-#     global df
-#     # 检查数据是否已加载
-#     if df is None:
-#         return 'Data not loaded yet', 400
-#     # 对 'PI' 字段进行计数统计
-#     PI_counts = Counter(df['PI'])
-#     # 取出前十个最常出现的PI值及其计数值，并将PI值转换为首字母大写
-#     top_10_PI = {pi.capitalize(): count for pi, count in PI_counts.most_common(10)}
-#     return jsonify(top_10_PI)
-
-
-
-
-
-
 
 
 load_data()
--- flask/demo.py
+++ flask/demo.py
@@ -1,40 +1,14 @@
-from flask import jsonify
-from collections import defaultdict
-import itertools
+from pymongo import MongoClient
 
-def cooccurrence_matrix_with_max_combination_json():
-    global df
+# 连接到 MongoDB
+client = MongoClient('localhost', 27017)
+db = client['pubmed']
+collection = db['pubmed_collection']
 
-    # 获取 MH 数据
-    mh_data = df['MH'].dropna().tolist()
+# 获取集合中的所有文档的字段名
+all_documents = collection.find({})
+field_names = set()
+for doc in all_documents:
+    field_names.update(doc.keys())
 
-    # 初始化共现矩阵
-    matrix = defaultdict(int)
-
-    # 遍历每个子列表
-    for terms in mh_data:
-        unique_terms = sorted(set(terms))
-        # 更新共现次数
-        for term1, term2 in itertools.combinations(unique_terms, 2):
-            matrix[(term1, term2)] += 1
-
-    # 找到共现次数前 20 的组合
-    top_20_combinations = sorted(matrix.items(), key=lambda x: x[1], reverse=True)[:20]
-
-    # 创建二维矩阵
-    matrix_data = [[""] * (len(top_20_combinations) + 1) for _ in range(len(top_20_combinations) + 1)]
-
-    # 添加行和列标签
-    matrix_data[0][0] = "Terms"
-    for i, (terms, _) in enumerate(top_20_combinations):
-        matrix_data[0][i+1] = f"{terms[0]} 和 {terms[1]}"
-        matrix_data[i+1][0] = f"{terms[0]} 和 {terms[1]}"
-
-    # 填充矩阵数据
-    for i, (terms, value) in enumerate(top_20_combinations):
-        term1, term2 = terms
-        matrix_data[i+1][i+1] = str(value)
-
-    return matrix_data
-x = cooccurrence_matrix_with_max_combination_json()
-print(x)
+print("All field names in the collection:", field_names)
